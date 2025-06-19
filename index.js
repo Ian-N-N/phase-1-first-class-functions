f102d5cc14677f49df02fa5ee1b4dcef4ccdb8e6
Ian Njuguna <ian.njuguna@student.moringaschool.com>
2025-06-19 22:24:56 +0300

    updated

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  receivesAFunction,
+  returnsANamedFunction,
+  returnsAnAnonymousFunction
+};
+
+function receivesAFunction(callback) {
+  callback(); 
+}
+function returnsANamedFunction() {
+  function namedFunction() {
+     return "I am a named function";
+  }
+     return namedFunction;
+}
+function returnsAnAnonymousFunction() {
+  return function () {
+     return "I am anonymous";
+  };
+}
+
+
